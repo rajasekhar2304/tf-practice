6324ac52c8640e9266dc2392eb8b36337c233bae
rajasekhar <raajasekharr@gmail.com>
2022-11-28 17:15:04 +0530

    Terraform Base Code

--- routing.tf
+++ routing.tf
@@ -1,22 +1,22 @@
 resource "aws_route_table" "terraform-public" {
- vpc_id = aws_vpc.testvpc01.id
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.testvpc01.id
- }
- tags = {
-   Name = "${var.vpc_name}-Main-RT"
- }
+  vpc_id = aws_vpc.testvpc01.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.testvpc01.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-Main-RT"
+  }
 }
 resource "aws_route_table_association" "terraform-publicsubnet-1" {
- subnet_id      = aws_subnet.subnet1-public.id
- route_table_id = aws_route_table.terraform-public.id
+  subnet_id      = aws_subnet.subnet1-public.id
+  route_table_id = aws_route_table.terraform-public.id
 }
 resource "aws_route_table_association" "terraform-publicsubnet-2" {
- subnet_id      = aws_subnet.subnet2-public.id
- route_table_id = aws_route_table.terraform-public.id
+  subnet_id      = aws_subnet.subnet2-public.id
+  route_table_id = aws_route_table.terraform-public.id
 }
 resource "aws_route_table_association" "terraform-publicsubnet-3" {
- subnet_id      = aws_subnet.subnet3-public.id
- route_table_id = aws_route_table.terraform-public.id
+  subnet_id      = aws_subnet.subnet3-public.id
+  route_table_id = aws_route_table.terraform-public.id
 }
--- subnet.tf
+++ subnet.tf
@@ -1,30 +1,29 @@
 resource "aws_subnet" "subnet1-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet1_cidr
- availability_zone = "us-east-2a"
- 
- tags = {
-   Name = "${var.public_subnet1_name}"
- }
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-1"
+  }
 }
- 
+
 resource "aws_subnet" "subnet2-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet2_cidr
- availability_zone = "us-east-2b"
- 
- tags = {
-   Name = "${var.public_subnet2_name}"
- }
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-2"
+  }
 }
- 
+
 resource "aws_subnet" "subnet3-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet3_cidr
- availability_zone = "us-east-2c"
- 
- tags = {
-   Name = "${var.public_subnet3_name}"
- }
- 
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet3_cidr
+  availability_zone = "us-east-2c"
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-3"
+  }
 }
