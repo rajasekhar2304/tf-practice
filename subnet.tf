9915df8f2ecea1e7a977196e929bb318700def23
rajasekhar <raajasekharr@gmail.com>
2022-11-28 17:27:27 +0530

    Terraform Base Code

--- subnet.tf
+++ subnet.tf
@@ -1,33 +1,33 @@
 resource "aws_subnet" "subnet1-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet1_cidr
- availability_zone = "us-east-2a"
- 
- tags = {
-   Name = "${var.vpc_name}-Public-Subnet-1"
-   Owner = "DevOpsB27"
- }
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name  = "${var.vpc_name}-Public-Subnet-1"
+    Owner = "DevOpsB27"
+  }
 }
- 
+
 resource "aws_subnet" "subnet2-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet2_cidr
- availability_zone = "us-east-2b"
- 
- tags = {
-   Name = "${var.vpc_name}-Public-Subnet-2"
-   Owner = "DevOpsB27"
- }
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name  = "${var.vpc_name}-Public-Subnet-2"
+    Owner = "DevOpsB27"
+  }
 }
- 
+
 resource "aws_subnet" "subnet3-public" {
- vpc_id            = aws_vpc.testvpc01.id
- cidr_block        = var.public_subnet3_cidr
- availability_zone = "us-east-2c"
- 
- tags = {
-   Name = "${var.vpc_name}-Public-Subnet-3"
-   Owner = "DevOpsB27"
- }
- 
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet3_cidr
+  availability_zone = "us-east-2c"
+
+  tags = {
+    Name  = "${var.vpc_name}-Public-Subnet-3"
+    Owner = "DevOpsB27"
+  }
+
 }
